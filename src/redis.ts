8ab505681aa92da92d90f914b3eaae3f8c44f1a4
bonustrack <fabien@bonustrack.co>
2022-02-01 17:54:14 +0700

    Add heartbeat

--- src/redis.ts
+++ src/redis.ts
@@ -6,7 +6,12 @@ let client;
   client = createClient({ url: process.env.DATABASE_URL });
   client.on('error', (err) => console.log(err));
   await client.connect();
-  setInterval(() => client.get('heartbeat'), 30e3);
+  setInterval(async () => {
+    const ts = (Date.now() / 1e3).toFixed();
+    await client.set('heartbeat', ts);
+    const heartbeat = await client.get('heartbeat');
+    console.log('Heartbeat', heartbeat)
+  }, 20e3);
 })();
 
 export default client;
