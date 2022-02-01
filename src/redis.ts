b5811e45b198b46384cff428e6312fb847f44d04
bonustrack <fabien@bonustrack.co>
2022-02-01 22:07:46 +0700

    Log redis states

--- src/redis.ts
+++ src/redis.ts
@@ -4,13 +4,17 @@ let client;
 
 (async () => {
   client = createClient({ url: process.env.DATABASE_URL });
-  client.on('error', (err) => console.log(err));
+  client.on('connect', () => console.log('Redis connect'));
+  client.on('ready', () => console.log('Redis ready'));
+  client.on('reconnecting', (err) => console.log('Redis reconnecting', err));
+  client.on('error', (err) => console.log('Redis error', err));
+  client.on('end', (err) => console.log('Redis end', err));
   await client.connect();
   setInterval(async () => {
     const ts = (Date.now() / 1e3).toFixed();
     await client.set('heartbeat', ts);
     const heartbeat = await client.get('heartbeat');
-    console.log('Heartbeat', heartbeat)
+    console.log('Heartbeat', heartbeat);
   }, 20e3);
 })();
 
