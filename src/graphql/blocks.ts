7201d8522f301aa521f55144e7e14b6a7a278a9e
Zuza Zuber <zuza@lewagon.org>
2022-10-26 16:01:52 +0200

    handle exceptions

--- src/graphql/blocks.ts
+++ src/graphql/blocks.ts
@@ -8,79 +8,87 @@ Object.keys(networks).forEach((network) => {
   starts[network] = networks[network].start || 1;
 });
 
+async function calculateBlockNum(network, ts) {
+  const provider = snapshot.utils.getProvider(network);
+  let [from, to] = await Promise.all([
+    provider.getBlock(starts[network] || 1),
+    provider.getBlock('latest')
+  ]);
+  if (ts > to.timestamp) return 'latest';
+  if (ts < from.timestamp) return 0;
+
+  let steps = 0;
+  let range = to.number - from.number;
+  while (range > 1) {
+    steps++;
+    // console.log('From', from.number, 'to', to.number);
+    // console.log('Range', range);
+    // console.log('Diff', from.timestamp - ts, to.timestamp - ts);
+
+    const blockNums: number[] = [];
+    const blockTime = (to.timestamp - from.timestamp) / (to.number - from.number);
+    const trialBlockNum = to.number - Math.ceil((to.timestamp - ts) / blockTime);
+    // console.log('Trial', trialBlockNum);
+
+    blockNums.push(trialBlockNum);
+    let leftSpace = Math.ceil((trialBlockNum - from.number) / 2);
+    let rightSpace = Math.ceil((to.number - trialBlockNum) / 2);
+    Array.from(Array(12)).forEach(() => {
+      blockNums.push(trialBlockNum - leftSpace);
+      blockNums.push(trialBlockNum + rightSpace);
+      leftSpace = Math.ceil(leftSpace / 2);
+      rightSpace = Math.ceil(rightSpace / 2);
+    });
+
+    let blocks: any[] = await Promise.all(
+      [...new Set(blockNums)]
+        .filter((blockNum) => blockNum > from.number && blockNum < to.number)
+        .map((blockNum) => provider.getBlock(blockNum))
+    );
+    blocks = [from, ...blocks, to].sort((a, b) => a.number - b.number);
+    // console.log(blocks.map((block: any) => block.number));
+
+    let newFrom = false;
+    let newTo = false;
+    blocks.forEach((block) => {
+      if (block.timestamp >= ts && !newTo) newTo = block;
+      if (block.timestamp <= ts) newFrom = block;
+    });
+    from = newFrom;
+    to = newTo;
+    range = to.number - from.number;
+  }
+
+  if (range === 0) console.log('Perfect match', network, ts);
+  // console.log('From', from.number, from.timestamp, 'to', to.number, to.timestamp);
+  console.log('Block', to.number, 'at', to.timestamp);
+  console.log('Target', ts, 'network', network, 'steps', steps);
+  return to.number;
+}
+
 async function tsToBlockNum(network, ts) {
+  const explorer_base_url = networks[network].explorer.apiUrl ?? networks[network].explorer.url;
+  const url = `${explorer_base_url}/api?module=block&action=getblocknobytime&timestamp=${ts}&closest=after`;
+
   try {
-    const explorer_base_url = network === '1' ? 'https://api.etherscan.io' : networks[network].explorer // TODO(zzuziak): figure out workaround for this
-    const url = `${explorer_base_url}/api?module=block&action=getblocknobytime&timestamp=${ts}&closest=after`
-    console.log("URL", url);
     const res = await fetch(url, {
       method: 'GET',
       headers: {
-        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
-      },});
+      }
+    });
     const responseData = await res.json();
-    console.log(responseData)
-    return responseData.result?.blockNumber ?? responseData.result; // TODO(zzuziak): run script for all networks to find exceptions
-  } catch (error) {
-    console.log("Explorer failed"); // TODO(zzuziak): remove me
-    console.log(error); // TODO(zzuziak): remove me unless logging needed
+    if (
+      responseData.status !== '1' ||
+      (responseData.result instanceof String && responseData.result.toLowerCase().includes('error'))
+    )
+      throw new Error(`API error message: ${responseData.message}, result: ${responseData.result}`);
 
-    const provider = snapshot.utils.getProvider(network); // TODO(zzuziak): refactor into calculateBlockNumber
-    let [from, to] = await Promise.all([
-      provider.getBlock(starts[network] || 1),
-      provider.getBlock('latest')
-    ]);
-    if (ts > to.timestamp) return 'latest';
-    if (ts < from.timestamp) return 0;
-
-    let steps = 0;
-    let range = to.number - from.number;
-    while (range > 1) {
-      steps++;
-      // console.log('From', from.number, 'to', to.number);
-      // console.log('Range', range);
-      // console.log('Diff', from.timestamp - ts, to.timestamp - ts);
-
-      const blockNums: number[] = [];
-      const blockTime = (to.timestamp - from.timestamp) / (to.number - from.number);
-      const trialBlockNum = to.number - Math.ceil((to.timestamp - ts) / blockTime);
-      // console.log('Trial', trialBlockNum);
-
-      blockNums.push(trialBlockNum);
-      let leftSpace = Math.ceil((trialBlockNum - from.number) / 2);
-      let rightSpace = Math.ceil((to.number - trialBlockNum) / 2);
-      Array.from(Array(12)).forEach(() => {
-        blockNums.push(trialBlockNum - leftSpace);
-        blockNums.push(trialBlockNum + rightSpace);
-        leftSpace = Math.ceil(leftSpace / 2);
-        rightSpace = Math.ceil(rightSpace / 2);
-      });
-
-      let blocks: any[] = await Promise.all(
-        [...new Set(blockNums)]
-          .filter((blockNum) => blockNum > from.number && blockNum < to.number)
-          .map((blockNum) => provider.getBlock(blockNum))
-      );
-      blocks = [from, ...blocks, to].sort((a, b) => a.number - b.number);
-      // console.log(blocks.map((block: any) => block.number));
-
-      let newFrom = false;
-      let newTo = false;
-      blocks.forEach((block) => {
-        if (block.timestamp >= ts && !newTo) newTo = block;
-        if (block.timestamp <= ts) newFrom = block;
-      });
-      from = newFrom;
-      to = newTo;
-      range = to.number - from.number;
-    }
-
-    if (range === 0) console.log('Perfect match', network, ts);
-    // console.log('From', from.number, from.timestamp, 'to', to.number, to.timestamp);
-    console.log('Block', to.number, 'at', to.timestamp);
-    console.log('Target', ts, 'network', network, 'steps', steps);
-    return to.number;
+    return Number(responseData.result.blockNumber) ?? Number(responseData.result);
+  } catch (error: any) {
+    console.log(error.message);
+    calculateBlockNum(network, ts);
   }
 }
 
