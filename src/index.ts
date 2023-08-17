05cc2a1c0c5df7dc1aac060bcf352253216c3b38
Wan <495709+wa0x6e@users.noreply.github.com>
2023-08-17 16:23:36 +0900

    chore: add metrics (#293)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import graphql from './graphql';
 import { initLogger, fallbackLogger } from '@snapshot-labs/snapshot-sentry';
+import graphql from './graphql';
+import initMetrics from './helpers/metrics';
 
 const app = express();
 const PORT = process.env.PORT || 3004;
 
 initLogger(app);
+initMetrics(app);
 
+app.disable('x-powered-by');
 app.use(express.json({ limit: '4mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
