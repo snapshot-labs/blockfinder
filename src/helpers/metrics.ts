29d023c54f148813975d7bd5464869c45b394c8a
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-17 12:03:01 +0900

    chore: add metrics

---
+++ src/helpers/metrics.ts
@@ -0,0 +1,8 @@
+import init from '@snapshot-labs/snapshot-metrics';
+import { Express } from 'express';
+
+export default function initMetrics(app: Express) {
+  init(app, {
+    whitelistedPath: [/^\/$/]
+  });
+}
