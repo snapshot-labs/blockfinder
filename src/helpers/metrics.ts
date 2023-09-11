2b3fd002c14dc0a49a9848c51203571dab6501df
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-09-11 22:56:45 +0900

    chore: upgrade metrics to 1.4.0

--- src/helpers/metrics.ts
+++ src/helpers/metrics.ts
@@ -1,9 +1,11 @@
 import init, { client } from '@snapshot-labs/snapshot-metrics';
+import { capture } from '@snapshot-labs/snapshot-sentry';
 import { Express } from 'express';
 
 export default function initMetrics(app: Express) {
   init(app, {
-    whitelistedPath: [/^\/$/]
+    whitelistedPath: [/^\/$/],
+    errorHandler: (e: any) => capture(e)
   });
 }
 
