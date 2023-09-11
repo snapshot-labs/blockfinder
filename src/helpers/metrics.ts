c4d004cb84191791c41cecce059ff747fbb0e6a5
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-09-12 02:32:44 +0900

    Revert "fix: instrument number of networks"
    This reverts commit de1d0884594fb064fe62ca0c3f70aeda92f0c404.

--- src/helpers/metrics.ts
+++ src/helpers/metrics.ts
@@ -1,7 +1,6 @@
 import init, { client } from '@snapshot-labs/snapshot-metrics';
 import { capture } from '@snapshot-labs/snapshot-sentry';
 import { Express } from 'express';
-import networks from '@snapshot-labs/snapshot.js/src/networks.json';
 
 export default function initMetrics(app: Express) {
   init(app, {
@@ -14,9 +13,3 @@ export const requestDeduplicatorSize = new client.Gauge({
   name: 'request_deduplicator_size',
   help: 'Total number of items in the deduplicator queue'
 });
-
-const networksCount = new client.Gauge({
-  name: 'networks_count',
-  help: 'Number of networks.'
-});
-networksCount.set(Object.keys(networks).length);
