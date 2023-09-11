de1d0884594fb064fe62ca0c3f70aeda92f0c404
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-09-12 02:32:32 +0900

    fix: instrument number of networks

--- src/helpers/metrics.ts
+++ src/helpers/metrics.ts
@@ -1,6 +1,7 @@
 import init, { client } from '@snapshot-labs/snapshot-metrics';
 import { capture } from '@snapshot-labs/snapshot-sentry';
 import { Express } from 'express';
+import networks from '@snapshot-labs/snapshot.js/src/networks.json';
 
 export default function initMetrics(app: Express) {
   init(app, {
@@ -13,3 +14,9 @@ export const requestDeduplicatorSize = new client.Gauge({
   name: 'request_deduplicator_size',
   help: 'Total number of items in the deduplicator queue'
 });
+
+const networksCount = new client.Gauge({
+  name: 'networks_count',
+  help: 'Number of networks.'
+});
+networksCount.set(Object.keys(networks).length);
