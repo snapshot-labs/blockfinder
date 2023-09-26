62a589535fed3db7435f6f94af1e78da80590387
Wan <495709+wa0x6e@users.noreply.github.com>
2023-09-26 23:57:51 +0900

    chore: setup test env (#291)
    * chore: setup test env
    
    * chore: add E2E tests
    
    * chore: remove unused property
    
    * chore: enable isolatedModules
    
    * chore: add types for supertest
    
    * chore: update command to reflect test scope
    
    * chore: increase test timeout

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/build/', '<rootDir>/test/fixtures/'],
+
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-node-single-context',
+  moduleFileExtensions: ['js', 'ts'],
+  testPathIgnorePatterns: ['build/'],
+  verbose: true,
+  transform: {
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        isolatedModules: true
+      }
+    ]
+  }
+};
