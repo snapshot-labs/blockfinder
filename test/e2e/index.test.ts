bdc360f8e6430656ef49f6cad60b1759fef1a6a3
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-09-27 16:51:44 +0900

    fix: throw meaninful error on invalid timestamp

--- test/e2e/index.test.ts
+++ test/e2e/index.test.ts
@@ -40,4 +40,69 @@ describe('POST /', () => {
       }
     });
   }, 15e3);
+
+  it('returns a GraphQLError when giving invalid network', async () => {
+    const response = await request(HOST)
+      .post('/')
+      .set('Content-type', 'application/json')
+      .send(
+        JSON.stringify({
+          query:
+            'query {  blocks (where: { ts: 1640000000, network_in: ["1", "4"] }) {network number}}',
+          variables: null
+        })
+      );
+
+    expect(response.body.errors).toEqual([
+      {
+        extensions: {
+          code: 'INVALID_NETWORK'
+        },
+        locations: [
+          {
+            column: 10,
+            line: 1
+          }
+        ],
+        message: 'invalid network',
+        path: ['blocks']
+      }
+    ]);
+  }, 15e3);
+
+  it.each([
+    ['in the future', Math.floor((Date.now() + 1000 * 60) / 1000)],
+    ['0', 0],
+    ['negative', -500]
+  ])(
+    'returns a GraphQLError when timestamp is %s',
+    async (title, ts) => {
+      const response = await request(HOST)
+        .post('/')
+        .set('Content-type', 'application/json')
+        .send(
+          JSON.stringify({
+            query: `query {  blocks (where: { ts: ${ts}, network_in: ["1"] }) {network number}}`,
+            variables: null
+          })
+        );
+
+      expect(response.body.errors).toEqual([
+        {
+          extensions: {
+            code: 'INVALID_TIMESTAMP'
+          },
+          locations: [
+            {
+              column: 10,
+              line: 1
+            }
+          ],
+          message: 'timestamp must be in the past',
+          path: ['blocks']
+        }
+      ]);
+    },
+    15e3
+  );
 });
