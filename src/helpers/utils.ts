5aff779c5de81248afa93595c2c4ec6f0df98fd7
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-05-30 18:44:13 +0400

    feat: add support for starknet network

---
+++ src/helpers/utils.ts
@@ -0,0 +1,36 @@
+import snapshot from '@snapshot-labs/snapshot.js';
+
+export type Block = {
+  number: number;
+  timestamp: number;
+};
+
+const STARKNET_NETWORKS = ['0x534e5f4d41494e', '0x534e5f5345504f4c4941'];
+
+export async function getBlockNumber(
+  network: string,
+  blockNum: number | 'latest'
+): Promise<Block> {
+  const provider = snapshot.utils.getProvider(network);
+  const result = await provider.getBlock(blockNum);
+
+  return interpretResult(network, result);
+}
+
+function interpretResult(network: string, result: any): Block {
+  if (!result) {
+    throw new Error(`Invalid block response`);
+  }
+
+  if (STARKNET_NETWORKS.includes(network)) {
+    return {
+      number: result.block_number,
+      timestamp: result.timestamp
+    };
+  }
+
+  return {
+    number: result.number,
+    timestamp: result.timestamp
+  };
+}
